168dde19782ab78663956b66dbdaab66416950ad
ajgitter54 <you@example.com>
2024-11-10 02:10:30 +0100

    aj, the website 2 Co-authored-by: awaheartemoji <awaheartemoji@users.noreply.github.com>

---
+++ main.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+	generateNewSplash();
+
+}
+// ily :3 -aj
+// ily :3 -ava
+
+generateNewSplash = function() {	
+	// 
+	const splashes = [
+		"Also play Half-Life!",
+		"beautiful big titty butt naked women dont fall out the sky you know",
+		"aj wuz heer",
+		"I LOVE AJJJJJ - awa",
+        "42",
+        "lllloooonnnnggggtttteeeexxxxtttt",
+        "tell em to bring out the whole ocean 🦀🐟🐠🐡🦈🦦🐬🦭🐳🐋🐙🦑🦐🐚🪸🪼🐌",
+        "Also play garn47, in theaters, ITS AWESOME!",
+        "Also play Slime Rancher!",
+        "Also play Slime Rancher 2!"
+	];
+
+	const randomSplash = splashes[Math.floor(Math.random() * splashes.length)];
+	const splashElement = document.getElementById("splash");	
+
+	splashElement.innerText = randomSplash;
+
+	// 
+	splashElement.style.fontSize = 50 - randomSplash.length + "px";
+}
